099021acfce18fb39275aa3d883ae46d274a78a3
gugl021 <gugl021@gmail.com>
2025-02-05 12:21:43 +0100

    Testing in incognito

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -30,7 +30,7 @@ function App() {
             It features offline support and can be installed on your device.
           </p>
           <p>
-            Here's a new version! Please work!
+            Here's a new version! Please work!!
           </p>
           <Button onClick={() => alert('Button clicked!')}>
             Try Me!
